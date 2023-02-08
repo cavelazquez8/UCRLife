5a1d747079926ba9fa45a557050c35ff5af7188d
Kenneth Salce <kenneth@ucr-secure-37-10-13-199-238.wnet.ucr.edu>
2023-02-08 13:41:03 -0800

    Added user controller with limited error checks

---
+++ backend/src/controllers/user.ts
@@ -0,0 +1,37 @@
+import {RequestHandler} from "express";
+import createHttpError from "http-errors";
+import UserModel from "../models/user";
+
+interface userSignUp{
+    username?: string,
+    firstName?: string,
+    email?: string,
+    password?: string,
+}
+
+export const usersignup: RequestHandler<unknown,unknown, userSignUp, unknown> = async (req, res, next) => {
+    const username = req.body.username;
+    //const firstName = req.body.firstName;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try{
+        if(!username || !email || !password){
+            throw createHttpError(400, "Parameters missing");
+        }
+        const existingUsername = await UserModel.findOne({username: username}).exec();
+        
+        if(existingUsername){
+            throw createHttpError(409, "Username already taken. Please create a new one.");
+        }
+
+        const existingEmail = await UserModel.findOne({email: email}).exec();
+        
+        if(existingEmail){
+            throw createHttpError(409, "Email already used. Please use another email.");
+        }
+    }
+    catch(error){
+        next(error);
+    }
+};  
