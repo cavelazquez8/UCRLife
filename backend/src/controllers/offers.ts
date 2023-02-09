6f7390c780d1919ea3789b1b6ae5fed9302a4746
gruiz2316 <gruiz031@ucr.edu>
2023-02-09 15:43:16 -0800

    error handling

--- backend/src/controllers/offers.ts
+++ backend/src/controllers/offers.ts
@@ -1,4 +1,6 @@
 import { RequestHandler } from "express";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 import offerModel from "../models/offer";
 
 export const getOffers: RequestHandler = async (req, res, next) => {
@@ -10,12 +12,45 @@ export const getOffers: RequestHandler = async (req, res, next) => {
     }
 };
 
-export const createOffer: RequestHandler = async (req, res, next) => {
+export const getOffer: RequestHandler = async (req, res, next) => {
+    const offerId = req.params.offerId;
+    
+    try {
+
+        if (!mongoose.isValidObjectId(offerId)) {
+            throw createHttpError(400, "Invalid offer id");
+        }
+
+        const offer = await offerModel.findById(offerId).exec();
+
+        if (!offer) {
+            throw createHttpError(404, "offer not found");
+        }
+
+        res.status(200).json(offer);
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+interface CreateOfferBody {
+    title?: string,
+    description?: string,
+    price?: number,
+}
+
+export const createOffer: RequestHandler<unknown, unknown, CreateOfferBody, unknown> = async (req, res, next) => {
     const title = req.body.title;
     const description = req.body.description;
     const price = req.body.price;
 
     try {
+
+        if (!title) {
+            throw createHttpError(400, "Offer must have a title");
+        }
+
         const newOffer = await offerModel.create({
             title: title,
             description: description,
