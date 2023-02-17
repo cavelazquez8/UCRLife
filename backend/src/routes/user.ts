55684f45c9c964134384bd925abb0a8526696f90
gruiz2316 <gruiz031@ucr.edu>
2023-02-16 17:42:26 -0800

    create offer frontend

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,10 +3,12 @@ import * as UserController from "../controllers/user";
 
 const router = express.Router();
 
-//router.get("/", UserController.getUsers);
-
 router.post("/signup", UserController.usersignup);
+
 router.post("/login", UserController.login);
+
 router.get("/", UserController.getVerifiedUser);
+
+router.post("/logout", UserController.logout);
 
 export default router;
