33db9ca20ab1ba17ddc123ede7190c70e3b09340
Kenneth Salce <kenneth@Kenneths-Air.lan>
2023-02-17 20:36:45 -0800

    Updated app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,94 +1,70 @@
-import React, { useEffect, useState }from 'react';
-import Col from 'react-bootstrap/esm/Col';
 import Container from 'react-bootstrap/esm/Container';
-import Row from 'react-bootstrap/esm/Row';
-import Offer from './components/offer';
-import {Offer as OfferModel} from "./models/offers"
-import styles from "./styles/OfferPage.module.css"
-import styleUtils from "./styles/utils.module.css"
-import * as OffersApi from "./network/offers_api";
-import AddOfferDialogue from './components/AddOfferDialogue';
-import { Button } from 'react-bootstrap';
+import LoginUserModel from './components/LoginUserModel';
+import NavBar from './components/NavBar';
 import SignUpModel from './components/SignUpModel';
+import { useEffect,useState } from 'react';
+import {User} from './models/user'
+import * as UserApi from './network/user_api'
+import OfferPageLoggedInView from './components/OfferPageLoggedInView';
+import OfferPageLoggedOutView from './components/OfferPageLoggedOutView';
 
 function App() {
-  const [offers, setOffers] = useState<OfferModel[]>([])
+  const[userLoggedIn, setLoggedInUser] = useState<User|null>(null);
+  const[showSignUpModel,setShowSignUpModel] = useState(false);
+  const[showLoginModel,setShowLoginModel] = useState(false);
 
-  const [showAddOfferDialoguel, setShowAddOfferDialogue] = useState(false);
-  const [showSignUpDialoguel, setShowSignUpDialogue] = useState(true);
-  const [offerToEdit, setOfferToEdit] = useState<OfferModel | null>(null);
-
-  useEffect(() => {
-    async function loadOffers(){
-      try {
-        const offers = await OffersApi.fetchOffers();
-        setOffers(offers);
-      } catch (error) {
-        console.error(error);
-        alert(error);
+  useEffect (() => {
+    async function fetchLoggedInUser(){
+      try{
+        const user = await UserApi.getLogIn();
+        setLoggedInUser(user);
       }
-      
+      catch(error){
+        console.error(error);
+      }
+      fetchLoggedInUser();
     }
-    loadOffers();
-      
-  }, []);
-
-  async function deleteOffer(offer: OfferModel) {
-    try {
-      await OffersApi.deleteOffer(offer._id);
-      setOffers(offers.filter(existingOffer => existingOffer._id !== offer._id));
-    } catch (error) {
-      console.error(error);
-      alert(error);
-    }
-  }
-
+  },[]);
   return (
+    <div>
+      <NavBar 
+          userLoggedIn = {userLoggedIn}
+          onSignupOption ={()=>setShowSignUpModel(true)}
+          onLoginOption ={()=>setShowLoginModel(true)}
+          onLogoutOption ={()=>setLoggedInUser(null)}
+
+      />
     <Container>
-      <Button className={`mb-4 ${styleUtils.Center}`}
-      onClick={() => setShowAddOfferDialogue(true)}>
-        Add New Offer
-      </Button>
-      <Row xs={1} md={2} xl={4} className="g-4">
-      {offers.map(offer => (
-        <Col key={offer._id}>
-        <Offer 
-        offer={offer} 
-        className={styles.offer} 
-        onOfferClicked={setOfferToEdit}
-        onDeleteOfferClicked={deleteOffer}
+      <>
+        {
+          userLoggedIn
+          ? <OfferPageLoggedInView/>
+          : <OfferPageLoggedOutView/>
+        }
+      </>
+
+    </Container>
+    {
+        showSignUpModel &&
+        <SignUpModel
+            onDismiss={()=>setShowSignUpModel(false)}
+            onSuccessSignUp={(user)=>{
+                setLoggedInUser(user);
+                setShowSignUpModel(false);
+            }}
         />
-        </Col>
-      ))}
-      </Row>
-      {showAddOfferDialoguel &&
-        <AddOfferDialogue
-          onDismiss={() => setShowAddOfferDialogue(false)}
-          onOfferSaved={(newOffer) => {
-            setOffers([...offers, newOffer]);
-            setShowAddOfferDialogue(false);
+      }
+      {
+        showLoginModel &&
+        <LoginUserModel 
+          onDismiss={()=>setShowLoginModel(false)}
+          onSuccessLogin={(user)=>{
+            setLoggedInUser(user);
+            setShowLoginModel(false);
           }}
         />
       }
-      {offerToEdit &&
-      <AddOfferDialogue 
-      offerToEdit={offerToEdit}
-      onDismiss={() => setOfferToEdit(null)}
-      onOfferSaved={(updatedOffer) => {
-        setOffers(offers.map(exisitngOffer => exisitngOffer._id === updatedOffer._id ? updatedOffer : exisitngOffer));
-        setOfferToEdit(null);
-      }}
-      />
-      }
-      {
-        showSignUpDialoguel &&
-        <SignUpModel
-            onDismiss={()=> setShowSignUpDialogue(false)}
-            onSuccessSignUp={()=>{}}
-        />
-      }
-
-    </Container>
+    </div>
   );
 }
 
