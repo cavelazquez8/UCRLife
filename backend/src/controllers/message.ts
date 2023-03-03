868f2a947b1bfe1c42834395e69b7bc59ec81a17
Kenneth Salce <kenneth@Kenneths-Air-2.lan>
2023-03-02 20:05:21 -0800

    Updated message controllers & set strict to false

--- backend/src/controllers/message.ts
+++ backend/src/controllers/message.ts
@@ -1,15 +1,25 @@
 import { RequestHandler } from "express";
 import createHttpError from "http-errors";
-import mongoose from "mongoose";
+//import mongoose from "mongoose";
 import messageModel from "../models/message";
+import userModel from "../models/user";
 
-const sendMessage: RequestHandler = async (req, res, next) => {
-  const message = new messageModel({
-    sender: req.body.sender,
-    recipient: req.body.recipient,
-    text: req.body.text
-  });
+
+export const sendMessage: RequestHandler = async (req, res, next) => {
   try {
+    const currentUser = req.session.userID;
+    if(!currentUser){
+      throw createHttpError(401,"Please login to send message");
+    }
+    const user = await userModel.findById(currentUser).select("+username");
+    const message = new messageModel({
+      sender: user.username,
+      recipient: req.body.recipient,
+      text: req.body.text
+    });
+    if(!message.recipient || !message.sender){
+      throw createHttpError(401, "User does not exist");
+    }
     await message.save();
     res.status(201).json({ message });
   } catch (error) {
@@ -17,14 +27,46 @@ const sendMessage: RequestHandler = async (req, res, next) => {
   }
 };
 
-const getMessages: RequestHandler = async (req, res, next) => {
+export const getMessages: RequestHandler = async (req, res, next) => {
   try {
-    const messages = await messageModel.find({ recipient: req.query.recipient });
+    const currentUser = req.session.userID;
+    const reciever = req.body.recipient;
+    if(!currentUser){
+      throw createHttpError(401, "Please login to see messages");
+    }
+    const user = await userModel.findById(currentUser).select("+username");
+    const messages = await messageModel.find({$or:[{ sender: user.username, recipient: reciever},{sender: reciever, recipient: user.username }]}).sort({"timestamp":-1}).exec();
     res.status(200).json({ messages });
   } catch (error) {
     next(error);
   }
 };
 
-export { sendMessage, getMessages };
+export const getUserInteractions: RequestHandler = async (req,res, next) =>{
+    try{
+      const currentUser = req.session.userID;
+      if(!currentUser){
+        throw createHttpError(401, "Please login to see your messages");
+      }
+      const user = await userModel.findById(currentUser).select("+username");
+      if(!user.username){
+        throw createHttpError(401, "Username does not exist");
+      }
+      /*const userRecievers = await messageModel.find({"$where": "this. username IN (select distinct sender  from messageModel where recipient  == this. currentUser.username  UNION  select distinct recipient from message where sender = currentUser.username"
+    },{
+       "username": 1
+    });*/
+    const userRecievers = await messageModel.find({$or:[{ sender: user.username},{recipient: user.username }]});
+    //const userRecievers = await userModel.aggregate([{
+        /*$lookup:{
+            from: "messageModel"*/
+        //}
+    //}])
+      res.status(200).json({userRecievers});
+    }
+    catch(error){
+      next(error);
+    }
+  
+};
 
