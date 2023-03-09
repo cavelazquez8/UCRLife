c02a0fe21ae99440d7bf7a462644899317172a4b
cavelazquez8 <cavelazquez8@gmail.com>
2023-03-08 18:18:59 -0800

    added rateOffer function

--- frontend/src/network/offers_api.ts
+++ frontend/src/network/offers_api.ts
@@ -69,3 +69,17 @@ export async function searchOffer(keyword: string): Promise<Offer[]> {
 export async function deleteOffer(offerId: string) {
 	await fetchData('/api/offers/' + offerId, { method: 'DELETE' });
 }
+
+export async function rateOffer(
+	offerId: string,
+	star: number,
+	comment: string,
+): Promise<void> {
+	await fetchData(`/api/offers/rating/${offerId}`, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ star, comment }),
+	});
+}
